79045121eae028a88eb0e38480122efaa3ff86d8
Zyffer2 <jhonwillsonq@gmail.com>
2026-03-30 21:26:03 +0200

    hello

--- nvim/.config/nvim/lua/Zyffer/plugins/telescope.lua
+++ nvim/.config/nvim/lua/Zyffer/plugins/telescope.lua
@@ -4,6 +4,10 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
     },
 
     keys = {
@@ -11,6 +15,7 @@ return {
       { "<leader>fg", function() require("telescope.builtin").live_grep() end },
       { "<leader>fb", function() require("telescope.builtin").buffers() end },
       { "<leader>fh", function() require("telescope.builtin").help_tags() end },
+      { "<leader>b",  function() require("telescope.builtin").builtin() end },
     },
 
     config = function()
@@ -18,12 +23,23 @@ return {
       local themes = require("telescope.themes")
 
       telescope.setup({
+        pickers = {
+          find_files = themes.get_ivy({
+          })
+        },
         extensions = {
           ["ui-select"] = themes.get_dropdown({}),
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
         },
       })
 
       pcall(telescope.load_extension, "ui-select")
+      pcall(telescope.load_extension, "fzf")
     end,
   },
 }
