5069fa8d2053388a14404672639e920dba5a7841
Zyffer2 <jhonwillsonq@gmail.com>
2026-06-04 14:38:54 +0200

    hello

--- git.sh
+++ git.sh
@@ -2,8 +2,7 @@
 
 git add .
 git commit -m "hello"
-git push -u origin main -f
-git push -u gitlab main -f
+git push -u origin main -f & git push -u gitlab main -f
 
 mkdir -p "$HOME/bk"
 cp -r "$PWD" "$HOME/bk"
