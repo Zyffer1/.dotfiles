69363738fbb5320eabc3f012bb5797d9eb6d59d9
Zyffer2 <jhonwillsonq@gmail.com>
2026-06-04 14:34:47 +0200

    Update dotfiles

--- stow.sh
+++ stow.sh
@@ -2,7 +2,7 @@
 
 cd "$HOME/.dotfiles" || exit 1
 
-stow -t ~ i3 zsh nvim tmux wezterm spicetify neofetch kitty
+stow -t ~ i3 zsh nvim tmux wezter
 sudo stow -t /root/ zsh nvim
 
 target="/etc/keyd/default.conf"
