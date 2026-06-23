581a1414d567552ff80abfece95bf21d196f25eb
Zyffer1 <jhonwillsonq@gmail.com>
2026-06-23 13:20:54 +0200

    hello

--- stow.sh
+++ stow.sh
@@ -3,7 +3,6 @@
 cd "$HOME/me/.dotfiles" || exit 1
 
 stow -t ~ i3 zsh nvim tmux wezterm bat opencode bash picom vim
-sudo stow -t /root/ zsh nvim
 
 target="/etc/keyd/default.conf"
 source="$HOME/me/.dotfiles/default.conf"
