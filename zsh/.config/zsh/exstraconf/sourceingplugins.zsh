f70c27d07ffdc085eda4f5622c75f4cb6323d15f
Zyffer1 <jhonwillsonq@gmail.com>
2026-06-27 01:42:48 +0200

    hello

--- zsh/.config/zsh/exstraconf/sourceingplugins.zsh
+++ zsh/.config/zsh/exstraconf/sourceingplugins.zsh
@@ -1,6 +1,5 @@
 source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source ~/.config/zsh/plugins/git.plugin.zsh
 eval "$(zoxide init --cmd cd zsh)"
 source <(fzf --zsh)
 [ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
