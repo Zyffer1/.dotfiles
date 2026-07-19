06a506b8b963e3a935bc75eb374ddf5a0f2e4656
Zyffer1 <jhonwillsonq@gmail.com>
2026-07-19 20:17:04 +0000

    hello

--- git.sh
+++ git.sh
@@ -5,7 +5,3 @@ sudo cp /etc/portage/make.conf ~/me/personal/.dotfiles/gentoo/
 git add .
 git commit -m "hello"
 git push -u github main && git push -u gitlab main 
-
-mkdir -p "$HOME/bk"
-cp -r "$PWD" "$HOME/bk"
-
