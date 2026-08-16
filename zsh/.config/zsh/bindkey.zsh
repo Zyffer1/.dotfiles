80eb915ead566c445ae27451c578acb87bc842e0
Zyffer1 <jhonwillsonq@gmail.com>
2026-08-16 13:34:36 +0200

    hello

--- zsh/.config/zsh/bindkey.zsh
+++ zsh/.config/zsh/bindkey.zsh
@@ -1,2 +1,10 @@
 bindkey -M viins '^[[3~' delete-char
 bindkey -M vicmd '^[[3~' delete-char
+
+function clear-screen-command() {
+    clear
+    zle redisplay
+}
+
+zle -N clear-screen-command
+bindkey '^L' clear-screen-command
